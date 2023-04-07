cfc685663c51a8ff100b40b1a7555388c16d055f
oliver <153884673@qq.com>
2023-04-07 16:20:34 +0800

    build(deps): 新增全局项目依赖

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,18 @@ import 'uno.css'
 import 'virtual:svg-icons-register'
 
 import { createApp } from 'vue'
+import Nprogress from 'nprogress'
 import App from './App.vue'
 import { setupStore } from './store'
 import { setupRouter } from './router'
+
+/**
+ * @description 导入加载进度条，防止首屏加载时间过长，用户等待
+ *
+ * */
+import 'nprogress/nprogress.css'
+Nprogress.configure({ showSpinner: false, ease: 'ease', speed: 500 })
+Nprogress.start()
 
 async function setupApp() {
   const app = createApp(App)
